e90b0272f043fe1d75999ea38c36a9e455482b70
Dominik George <nik@naturalnet.de>
2016-08-02 22:40:34 +0200

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# ~*~ coding: utf-8 ~*~
+
+from setuptools import setup, find_packages
+
+setup(
+    # Basic information
+    name = 'OSMAlchemy',
+    version = '0.1',
+    license = 'MIT',
+    keywords = 'osm openstreetmap proxy caching orm',
+    description = 'OpenStreetMap to SQLAlchemy bridge',
+    url = 'https://github.com/Natureshadow/OSMAlchemy',
+
+    # Author information
+    author = 'Dominik George',
+    author_email = 'nik@naturalnet.de',
+
+    # Included code
+    packages = find_packages(),
+
+    # Distribution information
+    zip_safe = True,
+    install_requires = [
+                        'SQLAlchemy',
+                       ],
+    classifiers = [
+                   'Development Status :: 3 - Alpha',
+                   'Environment :: Plugins',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Topic :: Database',
+                   'Topic :: Scientific/Engineering :: GIS',
+                   'Topic :: Software Development :: Libraries :: Python Modules'
+                  ]
+)
